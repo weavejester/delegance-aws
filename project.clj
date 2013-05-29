3d8dde404f379068163527ebb681b04bd28dd7cd
James Reeves <jreeves@weavejester.com>
2013-05-29 11:41:35 +0100

    Updated Delegance dependency to 0.1.1

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [delegance "0.1.0"]
+                 [delegance "0.1.1"]
                  [com.cemerick/rummage "1.0.1"]
                  [com.cemerick/bandalore "0.0.3"]])
